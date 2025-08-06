89b84aed875cdf96f5af01ac88c47c75949f3b72
Ankana Majee <majeeankana4@gmail.com>
2025-08-07 00:44:02 +0530

    DSA practice

--- DSA/leetcode_442.py
+++ DSA/leetcode_442.py
@@ -1,11 +1,11 @@
 name = [4, 3, 2, 7, 8, 2, 3, 1]
-# name = [1]
+name = [1]
 
-# temp_list = []
-# for i in name:
-#     if name.count(i) == 2 and i not in temp_list:
-#         temp_list.append(i)   
-# print(temp_list)            
+temp_list = []
+for i in name:
+    if name.count(i) == 2 and i not in temp_list:
+        temp_list.append(i)   
+print(temp_list)            
         
 temp_dict = {}
 for i in name:
@@ -18,3 +18,4 @@ for k, v in temp_dict.items():
     if v == 2:
         result.append(k)
 print(result) 
+
