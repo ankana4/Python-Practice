d5a1c9b29a4235bcd1c923d74dd7b15ee89482cf
ankana4 <majeeankana4@gmail.com>
2026-08-05 16:06:52 +0530

    Add examples for slicing operations in lists

---
+++ List/slicing.py
@@ -0,0 +1,26 @@
+#First three elements
+#Extract the first three elements.
+numbers = [10, 20, 30, 40, 50, 60]
+print(numbers[:3])
+
+
+#Last three elements
+#Extract the last three elements.
+print(numbers[-3:])
+
+#Index 1 to 4
+#Extract elements from index 1 through index 4, inclusive
+print(numbers[1:5])
+
+#Every second element
+#Extract every second element.
+print(numbers[1::2])
+
+#Odd indexes
+#Extract elements at odd indexes.
+print(numbers[1::2])
+
+#Reverse with slicing
+#Reverse the list using slicing.
+print(numbers[::-1])
+
