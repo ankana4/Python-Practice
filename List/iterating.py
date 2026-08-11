6be5fa3f97831a54e15fa7e934c763d48df2e405
ankana4 <majeeankana4@gmail.com>
2026-08-11 16:50:03 +0530

    Refactor iteration examples and add conditional printing for values greater than 20

--- List/iterating.py
+++ List/iterating.py
@@ -25,3 +25,28 @@ for i, n in enumerate(numbers):
 numbers = [10, 20, 30, 40]
 for i in range(len(numbers)-1, -1, -1):
     print(numbers[i])
+    
+    
+#Conditional printing
+#Print only values greater than 20.   
+numbers = [10, 20, 30, 40]
+for i in range(len(numbers)):
+    if numbers[i] > 20:
+        print(numbers[i]) 
+    
+    
+#Even indexes
+#Print values at even indexes. Do not confuse even indexes with even values.  
+for i in range(0, len(numbers), 2):
+    print(f"Numver at index {i} is {numbers[i]}")  
+    
+
+
+#Stop at first negative
+#Use break to stop before printing the negative number.
+numbers = [10, 20, 30, -5, 40, 50]    
+
+for i in range(len(numbers)):
+    if numbers[i] < 0:
+        break
+    print(numbers[i])
