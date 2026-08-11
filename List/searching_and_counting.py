0191a22f59c190856bc722d420b3f42e30c4d99e
ankana4 <majeeankana4@gmail.com>
2026-08-11 17:01:04 +0530

    finding last index

--- List/searching_and_counting.py
+++ List/searching_and_counting.py
@@ -1,7 +1,15 @@
 #First occurrence
 #Find the index of the first 7.
-numbers = [4, 2, 2, 7, 9]
+numbers = [4, 7, 2, 7, 9]
 for i in range(len(numbers)):
+    if numbers[i] == 7:
+        print(i)
+        break
+    
+
+#Last occurrence
+#Find the index of the last 7 without using a reverse-index shortcut.    
+for i in range(len(numbers)-1, -1, -1):
     if numbers[i] == 7:
         print(i)
         break
