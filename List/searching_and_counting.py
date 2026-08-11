d63e6b95ccbfd52acf6ea6689cf5773ec4ef9439
ankana4 <majeeankana4@gmail.com>
2026-08-11 17:02:11 +0530

    all occurance indexes

--- List/searching_and_counting.py
+++ List/searching_and_counting.py
@@ -13,3 +13,10 @@ for i in range(len(numbers)-1, -1, -1):
     if numbers[i] == 7:
         print(i)
         break
+    
+#All occurrence indexes
+#Return [1, 3, 5].
+numbers = [4, 7, 2, 7, 9, 7]   
+for i in range(len(numbers)):
+    if numbers[i] == 7:
+        print(i)
