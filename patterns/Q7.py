5d80651e5b27774a2b2b95e3c7bbf26584e0f1f2
ankana4 <majeeankana4@gmail.com>
2026-08-12 20:42:46 +0530

    Add functionality for printing inverted pyramid pattern

--- patterns/Q7.py
+++ patterns/Q7.py
@@ -14,4 +14,23 @@ for i in range(0, n):
     for l in range(0, n-i-1):
         print(" ",end="")
     print()
+    
+    
+ 
+'''
+    *
+   * *
+  * * *
+ * * * *  
+'''
+n = 5
+for i in range(0, n):
+    for j in range(0, n-i-1):
+        print(" ", end="")
+    for k in range(0, i+1):
+        print("* ", end="")
+    for l in range(0, n-i-1):
+        print(" ",end="")
+    print()
+    
                 
