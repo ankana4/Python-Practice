7bb330e0f658206d40f9c4e86c77a8e2f2301d43
ankana4 <majeeankana4@gmail.com>
2026-08-13 18:05:06 +0530

    Add functionality for printing a centered inverted pyramid pattern

---
+++ patterns/Q8.py
@@ -0,0 +1,16 @@
+'''
+ *********
+  *******
+   *****
+    ***
+     *
+'''
+n = 5
+for i in range(0, n):
+    for j in range(0, i):
+        print(" ", end="")
+    for j in range(0, 2*n-(2*i+1)):
+        print("*", end="")
+    for k in range(0, i):
+        print(" ", end="")
+    print()            
