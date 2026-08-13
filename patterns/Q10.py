963ef1f131203a9968a5ba64b91230b58a504325
ankana4 <majeeankana4@gmail.com>
2026-08-13 18:41:02 +0530

    Add functionality for printing a pyramid and inverted pyramid pattern

---
+++ patterns/Q10.py
@@ -0,0 +1,19 @@
+'''
+*
+**
+***
+****
+***
+**
+*
+'''
+n = 5
+for i in range(0, n):
+    for j in range(0, i+1):
+        print("*", end="")
+    print()    
+
+for i in range(0, n):
+    for j in range(0, n-i-1):
+        print("*", end="")
+    print()
