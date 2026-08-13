91f53bc43a562bc1d57c583ac0a23727506cef57
ankana4 <majeeankana4@gmail.com>
2026-08-13 18:11:28 +0530

    Add functionality for printing a centered decreasing pyramid pattern

--- patterns/Q8.py
+++ patterns/Q8.py
@@ -14,3 +14,22 @@ for i in range(0, n):
     for k in range(0, i):
         print(" ", end="")
     print()            
+    
+    
+
+'''
+********
+ ******
+  ****
+   **
+   
+'''
+n = 5
+for i in range(0, n):
+    for j in range(0, i):
+        print(" ", end="")
+    for j in range(0, 2*n-(2*i+2)):
+        print("*", end="")
+    for j in range(0, i):
+        print(" ", end="")
+    print()            
