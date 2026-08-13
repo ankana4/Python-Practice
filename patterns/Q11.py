a878adac75c1625a31475461c77f50bbe8be320f
ankana4 <majeeankana4@gmail.com>
2026-08-13 19:02:04 +0530

    Add functionality for printing a binary pattern triangle

---
+++ patterns/Q11.py
@@ -0,0 +1,20 @@
+'''
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+
+'''
+n = 5
+start = 1
+for i in range(0, n):
+    if i%2==0:
+        start = 1
+    else:
+        start = 0
+    for j in range(0, i+1):
+        print(start, end="")
+        start = 1-start
+    print()        
+    
