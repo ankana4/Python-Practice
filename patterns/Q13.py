df9f3573f6c5e0b3965b864043fdc85ea12f52dd
ankana4 <majeeankana4@gmail.com>
2026-08-17 19:54:37 +0530

    practicing pattern

---
+++ patterns/Q13.py
@@ -0,0 +1,15 @@
+'''
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+'''
+n = 6
+num = 1
+for i in range(0, n):
+    for j in range(0, i):
+        print(num, end=" ")
+        num += 1
+    print()    
+print()        
