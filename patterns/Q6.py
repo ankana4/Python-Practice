fb5adbc1743c8f4d297c52a1b39667e815d72214
ankana4 <majeeankana4@gmail.com>
2026-08-18 18:36:47 +0530

    Add alternate pattern printing approach in Q6.py

--- patterns/Q6.py
+++ patterns/Q6.py
@@ -10,3 +10,12 @@ for i in range(n, 0, -1):
     for j in range(1, i+1):
         print(j, end="")
     print()    
+    
+#Another approach
+num = 1
+for i in range(0, n):
+    num = 1
+    for j in range(n, i, -1):
+        print(num, end="")
+        num += 1
+    print()        
