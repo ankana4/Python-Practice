67fcbe615dc0c00c4931082bf2748cf30b7dd7aa
ankana4 <majeeankana4@gmail.com>
2026-08-18 13:02:25 +0530

    Add diamond and pyramid pattern printing script in Q19.py

---
+++ patterns/Q19.py
@@ -0,0 +1,35 @@
+'''
+**********
+**** ****
+***   ***
+**     **
+*       *
+*       *
+**     **
+***   ***
+**** ****
+**********
+'''
+
+n = 5
+for i in range(0, n):
+    for j in range(n, i, -1):
+        print("*", end="")   
+    
+    for j in range(0, 2*i+1):
+        print(" ",end="")
+        
+    for j in range(n, i, -1):
+            print("*", end="")
+    print()     
+
+for i in range(0, n):
+    for j in range(0, i+1):
+        print("*", end="")
+    
+    for j in range(0, 2*n-(2*i+1)):
+        print(" ",end="")
+        
+    for j in range(0, i+1):
+            print("*", end="")
+    print()        
