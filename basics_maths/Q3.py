3da9ec076c71b1a026e211238c963cdcd1a39f8a
ankana4 <majeeankana4@gmail.com>
2026-08-19 18:59:03 +0530

    Add alternative palindrome checking function in Q3.py

--- basics_maths/Q3.py
+++ basics_maths/Q3.py
@@ -11,3 +11,19 @@ if rev == compare:
 else:
     print("Not Palindrome")        
   
+  #Another approach
+  
+def is_palindrome(n):
+    rev = 0
+    dup = n
+    while n>0:
+        last_digit = n%10
+        rev = rev*10+last_digit
+        n = n//10  
+    return rev == dup
+
+n = 455
+if is_palindrome(n):
+    print("Palindrome")
+else:
+    print("Not Palindrome")    
