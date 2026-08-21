04925d1299d3e86ce8ae4d35cbfceff8b09a71de
ankana4 <majeeankana4@gmail.com>
2026-08-21 18:02:28 +0530

    Refactor digit counting logic and add alternative approach in Q1.py

--- basics_maths/Q1.py
+++ basics_maths/Q1.py
@@ -1,4 +1,5 @@
 #Count of digits in a number
+import math
 
 numbers = 32145
 c=0
@@ -6,3 +7,14 @@ while numbers>0:
     c+=1
     numbers=numbers//10
 print(c)
+
+#Alternate approach
+def CountDigits(numberts):
+    cnt = int(math.log10(numbers)+1)
+    
+    return cnt
+
+if "__main__" == __name__:
+    numbers = 32145
+    print(CountDigits(numbers))
+    
