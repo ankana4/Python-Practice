212ee154f6c1efc0cff0b960672e817066ace57c
ankana4 <majeeankana4@gmail.com>
2026-08-21 18:06:39 +0530

    Add optimized GCD function and update input handling in Q4.py

--- basics_maths/Q4.py
+++ basics_maths/Q4.py
@@ -10,3 +10,15 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number "))
 
 print(find_gcd(num1, num2))        
+
+#Optimize approach
+def find_gcd_of_number(n1, n2):
+    for i in range(min(n1, n2), 0, -1):
+        if n1%i == 0 and n2%i == 0:
+            return i
+    return 1
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number "))
+
+print(find_gcd_of_number(num1, num2))
