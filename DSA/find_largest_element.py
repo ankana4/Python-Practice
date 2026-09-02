33cf1fc3e223a74bab494ed8ade697bc631655c1
ankana4 <majeeankana4@gmail.com>
2026-09-02 12:56:59 +0530

    Find largest element approaching using brute-force approach

---
+++ DSA/find_largest_element.py
@@ -0,0 +1,17 @@
+#Find the Largest element in an array
+#1.Using Bubble sort approach - brute force appraoch
+
+nums = [2, 5, 1, 3, 0]
+largest = nums[0]
+n = len(nums)
+for i in range(0, n):
+    didswapped=False
+    for j in range(0, n-i-1):
+        if nums[j] < nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+            didswapped=True
+    if not didswapped:
+        break
+print(nums)
+print(nums[0])            
+            
