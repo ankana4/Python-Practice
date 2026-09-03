7ee59f38e0b32dfc501095d33f68a26029d5d228
ankana4 <majeeankana4@gmail.com>
2026-09-03 13:04:46 +0530

    add brute-force approach to remove duplicates in-place from sorted array

---
+++ DSA/remove_duplicates.py
@@ -0,0 +1,15 @@
+#Remove Duplicates in-place from Sorted Array
+#Brute-force approach
+
+nums = [1,1,2,2,2,3,3]
+output = []
+
+for i in nums:
+    if i not in output:
+        output.append(i)
+add_digit = len(nums) - len(output)
+for j in range(0, add_digit):
+    output.append(0)
+print(output)            
+
+
