53fdbc50b3a8cdb462d12fee29b7643c9bbb9c1b
ankana4 <majeeankana4@gmail.com>
2026-09-03 18:21:53 +0530

    add brute-force approach to rotate an array by K elements

---
+++ DSA/rotate_array_by_k.py
@@ -0,0 +1,22 @@
+#Rotate array by K elements
+#brute-force approach
+arr = [1, 2, 3, 4, 5, 6, 7]
+n = len(arr)
+d=3
+temp = []
+d = d%n
+
+#stored first d elements
+for i in range(0, d):
+    temp.append(arr[i])
+print(temp)    
+
+#shift remaining elements to the left
+for i in range(d, n):
+    arr[i-d] = arr[i]
+print(temp)    
+
+#put temp variables at the end
+for i in range(n-d, n):
+    arr[i] = temp[i-(n-d)]
+print(arr)    
