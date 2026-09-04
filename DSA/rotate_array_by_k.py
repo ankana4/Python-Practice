760067e024e7d6668da8e67410f8c1cc24b3be64
ankana4 <majeeankana4@gmail.com>
2026-09-04 09:04:07 +0530

    Another optimal approach

--- DSA/rotate_array_by_k.py
+++ DSA/rotate_array_by_k.py
@@ -35,3 +35,28 @@ arr[:d] = reversed(arr[:d])
 arr[d:] = reversed(arr[d:])
 
 print(arr)
+
+
+#Another optimal approach
+nums = [2, 3, 4, 5, 6, 7, 8, 9]
+def reverse(nums, start, end):
+    while start<end:
+        nums[start], nums[end]=nums[end], nums[start]
+        start += 1
+        end -= 1
+
+def rotateArray(nums, k, direction):
+    n = len(nums)
+    if n == 0 or k == 0:
+        print(nums)       
+    k=k%n
+    if direction == "right":
+        reverse(nums, 0, n-1)
+        reverse(nums, 0, k-1)
+        reverse(nums, k, n-1)
+    elif direction == "left":
+        reverse(nums, 0, k-1)
+        reverse(nums, k, n-1)
+        reverse(nums, 0, n-1)    
+    print(nums)  
+rotateArray(nums, 3, "left")           
