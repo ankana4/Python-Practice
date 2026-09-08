e6401cc093dc9de2b4bf12eb7e89a242aac60ad3
ankana4 <majeeankana4@gmail.com>
2026-09-08 15:20:51 +0530

    Implement two-pointer approach for union of two sorted arrays

--- DSA/union_of_two_sorted_arr.py
+++ DSA/union_of_two_sorted_arr.py
@@ -12,3 +12,37 @@ print(seen)
 #Another approach using set()
 st = set(arr1) | set(arr2)
 print(sorted(st))
+
+#Two-pointer approach
+m = len(arr1)
+n = len(arr2)
+i,j=0,0
+
+union_list=[]
+while i<m and j<n:
+    if arr1[i]<arr2[j]:
+        if not union_list or union_list[-1] != arr1[i]:
+            union_list.append(arr1[i])
+        i+=1
+        
+    elif arr2[j]<arr1[i]:
+        if not union_list or union_list[-1] != arr2[j]:
+            union_list.append(arr2[j])
+        j+=1    
+    
+    else:
+        if not union_list or union_list[-1] != arr1[i]:
+            union_list.append(arr1[i])
+        i+=1
+        j+=1    
+while i<n:
+    if not union_list or union_list[-1] != arr1[i]:
+            union_list.append(arr1[i])
+    i+=1
+while j<m:
+    if not union_list or union_list[-1] != arr2[j]:
+                union_list.append(arr2[j])
+    j+=1
+
+print(union_list)    
+                            
