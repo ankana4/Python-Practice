a7c0817ec8b1128924daade9a62c8628c3d54e73
ankana4 <majeeankana4@gmail.com>
2026-09-10 19:50:27 +0530

    Refactor brute-force approach for longest subarray with sum K

--- DSA/longest_subarray.py
+++ DSA/longest_subarray.py
@@ -6,6 +6,20 @@ k = 15
 n = len(nums)
 l = 0
 for i in range(0, n):
+    for j in range(i, n):
+        s = 0
+        for x in range(i, j+1):
+            s += nums[x]
+        if s==k:
+            l=max(l, j-i+1)
+print(l)                
+
+
+nums = [10, 5, 2, 7, 1, 9]
+k = 15  
+n = len(nums)
+l = 0
+for i in range(0, n):
     s = 0
     for j in range(i, n):
         s += nums[j]
