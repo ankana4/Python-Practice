5a59827f92a194edb4ea8efff25ae04aca6beea5
ankana4 <majeeankana4@gmail.com>
2026-09-15 11:09:45 +0530

    Fix linear search for missing number by correcting array length and output logic

--- DSA/find_missing_number.py
+++ DSA/find_missing_number.py
@@ -3,17 +3,17 @@
 #Linear search for missing number
 
 arr= [8, 2, 4, 5, 3, 7, 1]
-n = len(arr)+1
-
+n = len(arr)
+data = -1
 for i in range(1, n):
     found = False
-    for j in range(0, n-1):
+    for j in range(0, n):
         if arr[j] == i:
             found = True
             break
     if not found:
-        print(i)
-print(-1)            
+        data=i
+print(data)            
 
 
 #Using set
