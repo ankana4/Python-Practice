080fffb4b81bdf0a7d3c1241bf96713b468f041c
ankana4 <majeeankana4@gmail.com>
2026-09-16 13:45:46 +0530

    use dutch national flag algorithm to sort an array of 0's, 1's and 2's

--- DSA/sort_an_array_of_0_1_2s.py
+++ DSA/sort_an_array_of_0_1_2s.py
@@ -1,7 +1,7 @@
 #Sort an array of 0s, 1s and 2s
 
 #Better approach
-arr = [1, 0, 2, 1, 0]
+arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0]
 n = len(arr)
 c0,c1,c2=0,0,0
 for i in arr:
@@ -18,3 +18,20 @@ for i in range(c0, c0+c1):
 for i in range(c0+c1, n):
     arr[i] = 2
 print(arr)                            
+
+#Optimal approach - Dutch National Flag Algorithm
+low = 0
+mid = 0
+high = n-1
+
+while mid<=high:
+    if arr[mid] == 0:
+        arr[mid], arr[low] = arr[low], arr[mid]
+        low+=1
+        mid+=1
+    elif arr[mid] == 1:
+        mid+=1
+    else:
+        arr[mid], arr[high] = arr[high], arr[mid]
+        high -=1
+print(arr)    
