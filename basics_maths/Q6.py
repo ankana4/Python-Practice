23f319eb9b97a9baf3a2a4fc754898fa884d7135
ankana4 <majeeankana4@gmail.com>
2026-09-17 09:18:44 +0530

    Add alternative method to find divisors using a while loop

--- basics_maths/Q6.py
+++ basics_maths/Q6.py
@@ -5,3 +5,14 @@ for i in range(1, N+1):
     if N%i==0:
         new_list.append(i)
 print(new_list)        
+
+#Another approach
+res = []
+i = 1
+while i*i <= N:
+    if N%i == 0:
+        res.append(i)
+        if N//i != i:
+            res.append(N//i)
+    i+=1        
+print(sorted(res))               
