162c57e79dd944c7e2615cd01aed2b5f4dd95509
ankana4 <majeeankana4@gmail.com>
2026-09-18 12:26:45 +0530

    Fix loop range in optimized GCD function to start from 1

--- basics_maths/Q4.py
+++ basics_maths/Q4.py
@@ -13,7 +13,7 @@ print(find_gcd(num1, num2))
 
 #Optimize approach
 def find_gcd_of_number(n1, n2):
-    for i in range(min(n1, n2), 0, -1):
+    for i in range(min(n1, n2), 1, -1):
         if n1%i == 0 and n2%i == 0:
             return i
     return 1
