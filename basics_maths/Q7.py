7f3b5310dfd46b402adc34c9e647aa594363890f
ankana4 <majeeankana4@gmail.com>
2026-09-18 11:40:48 +0530

    Update prime number check to use n = 6 and add alternative approach for verification

--- basics_maths/Q7.py
+++ basics_maths/Q7.py
@@ -1,5 +1,5 @@
 #Check if a number is prime or not
-n = 5
+n = 6
 c = 0
 for i in range(1, n+1):
     if n%i == 0:
@@ -8,3 +8,15 @@ if c == 2:
     print("Prime number")
 else:
     print("Not prime number")            
+ 
+#Another approach
+while i*i <= n:
+    if n%i == 0:
+        c+=1
+        if n//2 % i != i:
+            c+=1
+if c == 2:
+    print("Prime")
+else:
+    print("Not Prime")                
+        
