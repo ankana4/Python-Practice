4d35733a695205f8bfe4bbea972c499012091ddb
ankana4 <majeeankana4@gmail.com>
2026-09-21 10:09:48 +0530

    Refactor zero-moving logic to improve readability and maintainability

--- DSA/move_zeros.py
+++ DSA/move_zeros.py
@@ -20,10 +20,9 @@ n = len(arr)
 for i in range(0, n):
     if arr[i] == 0:
         j = i
-        break
-if j!=-1:    
-    for i in range(j+1, n):
-        if arr[i]!=0:
-            arr[i], arr[j] = arr[j], arr[i]
-            j += 1
+        break   
+for i in range(j+1, n):
+    if arr[i]!=0:
+        arr[i], arr[j] = arr[j], arr[i]
+        j += 1
 print(arr)            
