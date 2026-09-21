0ae3424e9794b83c068f037bcc45c6797b4e6af0
ankana4 <majeeankana4@gmail.com>
2026-09-21 21:28:24 +0530

    Refactor inner loop structure in zero-moving algorithm for clarity

--- DSA/move_zeros.py
+++ DSA/move_zeros.py
@@ -20,9 +20,10 @@ n = len(arr)
 for i in range(0, n):
     if arr[i] == 0:
         j = i
-        break   
-for i in range(j+1, n):
-    if arr[i]!=0:
-        arr[i], arr[j] = arr[j], arr[i]
-        j += 1
+        break  
+if j != -1:    
+    for i in range(j+1, n):
+        if arr[i]!=0:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
 print(arr)            
