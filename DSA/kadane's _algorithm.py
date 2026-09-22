2dee3778fc13839d4be622195c0092852b59f600
ankana4 <majeeankana4@gmail.com>
2026-09-22 10:06:50 +0530

    Add optimal approach using Kadane's algorithm for maximum subarray sum

--- DSA/kadane's _algorithm.py
+++ DSA/kadane's _algorithm.py
@@ -10,3 +10,26 @@ for i in range(0, n):
         s += arr[j]
         ml = max(ml, s)
 print(ml)        
+
+#Optimal approach - Kadane's algorithm
+arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+n = len(arr)
+maxi = float('-inf')
+summ = 0
+
+start = 0
+ansStart = -1
+ansEnd = -1
+
+for i in range(0, n):
+    if summ == 0:
+        start = i
+    summ += arr[i]
+    if summ > maxi:
+        maxi=summ
+        ansStart = start
+        ansEnd = i
+    if summ < 0:
+        summ = 0
+print(maxi)
+print("Subarray: ", arr[ansStart:ansEnd + 1])               
